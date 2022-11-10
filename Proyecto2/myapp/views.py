12a1c923a0185e60ebb6e6cf40112a3d37750338
EPachecoGS <eduardo.pacheco@sansano.usm.cl>
2022-11-10 18:09:48 -0300

    Main

--- Proyecto2/myapp/views.py
+++ Proyecto2/myapp/views.py
@@ -1,18 +1,15 @@
-from http.client import HTTPResponse
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
-from django.urls import reverse
-from .models import *
+#from http.client import HTTPResponse
+#from django.shortcuts import render
+#from django.http import HttpResponse, HttpResponseRedirect
+#from django.template import loader
+#from django.urls import reverse
+#from .models import *
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("Hello world")
-def CrearUsuario(request):
-    NombreUsuario = request.post["Usuario"]
-    contrasena= request.post["contrasena"]
-    usuario= personas(name=NombreUsuario, password=contrasena)
-    usuario.save()
-
-
-
+#def hello(request):
+#    return HttpResponse("Hello world")
+#def CrearUsuario(request):
+#    NombreUsuario = request.post["Usuario"]
+#    contrasena= request.post["contrasena"]
+#    usuario= personas(name=NombreUsuario, password=contrasena)
+#    usuario.save()
